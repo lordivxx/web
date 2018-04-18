5eca88953e8247def8da6b27f057f3a731300710
Chuck Forsyth <forsythc@ucr.edu>
2018-04-17 22:06:57 -0700

    published

--- publish-me.sh
+++ publish-me.sh
@@ -4,6 +4,10 @@ module load ruby
 
 jekyll build --source /rhome/forsythc/repos/jeykll --destination /rhome/forsythc/repos/web
 
+git add .
+git commit -m "published"
+git push
+
 cd /rhome/forsythc/repos/web
 
 git add .
